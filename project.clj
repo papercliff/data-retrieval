a9eeaeefa6b99a748136c3cfff1a69681dc7e9b5
mrdimosthenis <dimos86m@gmail.com>
2022-11-10 18:50:28 +0200

    Add main ns in project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :main data-retrieval.core
   :dependencies [[aysylu/loom "1.0.2"]
                  [clj-http "3.12.3"]
                  [clj-time "0.15.2"]
